2398d00b27221fe6802056d902a60622fa9fe0bd
Wesley <R3S3ARCH3R-6@users.noreply.github.com>
2020-04-10 21:56:06 -0500

    Gun Assets
    Imported and made assets for the player's gun

--- Team Stairways Final Project/Assets/Scripts/Character Controls and Actions/Player_Attack.cs
+++ Team Stairways Final Project/Assets/Scripts/Character Controls and Actions/Player_Attack.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
+    public GameObject defaultItem;
+    private GameObject[] itemList = new GameObject[4];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemList[0] = defaultItem;
+        Instantiate(defaultItem);
+        for(int i = 1; i < itemList.Length; i++)
+        {
+            itemList[i] = null;
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +23,6 @@ public class Player_Attack : MonoBehaviour
     {
         
     }
+
+
 }
