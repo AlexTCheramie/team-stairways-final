83d3cee4f75f689a0302f4260e5ce30b4ba6e6d6
Wesley Johnson <C00157698@louisiana.edu>
2020-04-25 19:51:18 -0500

    Bear Update
    Updated bear attack scripts (still not complete) and added colliders onto the bear stand-in.

--- Team Stairways Final Project/Assets/Scripts/Enemy Scripts/BearAI.cs
+++ Team Stairways Final Project/Assets/Scripts/Enemy Scripts/BearAI.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;     //enables the project to use Unity AI library
 
-
 /// <summary>
-/// The following script is very similar to the standard EnemyAI.cs script,
-/// but is designed specifically for the bear boss.
+/// The following script is derived from the enemy script given from 
+/// CMPS 327 and the Star Wars game
 /// </summary>
 
+//FSM States for the enemy
 public enum Enemy2State { CHASE, MOVING, DEFAULT, ATTACK };
-public class BearAI : MonoBehaviour
-{
+//Default - init state (when it is not moving)
+//other states will be based on the player's rotation/movement
+
+/// <summary>
+/// EnemyAI ...
+/// </summary>
+/// 
+[RequireComponent(typeof(NavMeshAgent))]
+public class BearAI : MonoBehaviour {
     GameObject player;  //the player
     NavMeshAgent agent; //used to move along the navmesh
 
@@ -22,22 +29,21 @@ public class BearAI : MonoBehaviour
     protected Vector3 destination = new Vector3(0, 0, 0);   //initial destination is the zero vector
 
 
-    public int enemyHealth = 3;
+    public int enemyHealth = 10;
 
     //Particle Sys. Explosion and blood spatter
-    ParticleSystem explosion;
-    bool explosionStarted = false;     //says whether the explosion has started or not (prevents repeats)
+    ParticleSystem blood;
+    bool spatterStarted = false;     //says whether the explosion has started or not (prevents repeats)
 
-    //EnemyShoot firePoint;
 
-    public float shootTime = 1.0f;
-    public float reloadTime = 1.0f;
+    Bear_Attack attack;
+    public float biteTime = 1.0f;
+    public float chewTime = 1.0f;
 
     public int killCount = 0;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         player = GameObject.FindWithTag("Player");
         //assigns anything with the tag "Player" in the scene to the object "player"
 
@@ -45,48 +51,41 @@ public class BearAI : MonoBehaviour
 
         agent = this.GetComponent<NavMeshAgent>();  //gets the NavMeshAgent component
 
-        explosion = GetComponentInChildren<ParticleSystem>();   //gets particle sys. component attached to the enemy
+        blood = GetComponentInChildren<ParticleSystem>();   //gets particle sys. component attached to the enemy
     }
 
     //creates a random position for the enemy to be in or go to
-    private Vector3 RandomPosition()
-    {
+    private Vector3 RandomPosition() {
         return new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f));
         //y-coord will be 0 (not instantiating in the air)
     }
 
-    void FaceTarget()
-    {
+    void FaceTarget() {
         Vector3 direction = (player.transform.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         //FSM control code below
-        switch (state)
-        {
+        switch (state) {
             //starts in the default state
             case Enemy2State.DEFAULT:
                 destination = transform.position + RandomPosition();
                 //add a random position to the enemy's position that the enemy will move to
 
                 //if the player is less than the "chaseDistance" from the enemy, switch to the "CHASE" state
-                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance) {
                     state = Enemy2State.CHASE;
-                }
-                else    //move the enemy randomly if the enemy is too far from the enemy
-                {
+                } else    //move the enemy randomly if the enemy is too far from the enemy
+                  {
 
                     state = Enemy2State.MOVING;
                     agent.SetDestination(destination);  //destination will be a random location
                 }
 
-                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance) {
                     state = Enemy2State.ATTACK;
                 }
                 break;
@@ -94,20 +93,17 @@ public class BearAI : MonoBehaviour
             case Enemy2State.MOVING:
                 //Debug.Log("Dest = " + destination);
                 //when enemy is < 5 from the random distance, change destination again (done in Default state)
-                if (Vector3.Distance(transform.position, destination) < 5)
-                {
+                if (Vector3.Distance(transform.position, destination) < 5) {
                     state = Enemy2State.DEFAULT;
                     //firePoint.shoot = false;
                 }
                 //if the enemy gets close enough to the player, switch to the chase state and chase the player
-                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance) {
                     state = Enemy2State.CHASE;
                     //firePoint.shoot = false;
                 }
 
-                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance) {
                     state = Enemy2State.ATTACK;
                     //firePoint.shoot = true;
                 }
@@ -116,15 +112,13 @@ public class BearAI : MonoBehaviour
             case Enemy2State.CHASE:
                 //FindObjectOfType<AudioManager>().Play("chase");
                 //if the distance b/w the enemy and player exceeds the chase distance, switch to Default state
-                if (Vector3.Distance(transform.position, player.transform.position) > chaseDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) > chaseDistance) {
                     //FindObjectOfType<AudioManager>().Play("lost");
                     state = Enemy2State.DEFAULT;
                     //firePoint.shoot = false;
                 }
 
-                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) <= attackDistance) {
                     state = Enemy2State.ATTACK;
                     //firePoint.shoot = true;
                 }
@@ -136,24 +130,21 @@ public class BearAI : MonoBehaviour
             case Enemy2State.ATTACK:
                 //FindObjectOfType<AudioManager>().Play("shoot");
 
-                if (Vector3.Distance(transform.position, player.transform.position) > attackDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) > attackDistance) {
                     //FindObjectOfType<AudioManager>().Play("get back");
                     state = Enemy2State.CHASE;
                     //firePoint.shoot = false;
                 }
 
-                if (Vector3.Distance(transform.position, player.transform.position) > chaseDistance)
-                {
+                if (Vector3.Distance(transform.position, player.transform.position) > chaseDistance) {
                     state = Enemy2State.DEFAULT;
                     //firePoint.shoot = false;
                 }
 
-                if (Time.time > shootTime)
-                {
+                if (Time.time > biteTime) {
                     //FindObjectOfType<AudioManager>().Play("blaster SFX");
                     //firePoint.FireBullet();
-                    shootTime = Time.time + reloadTime;
+                    biteTime = Time.time + chewTime;
                 }
                 FaceTarget();
                 agent.SetDestination(player.transform.position);
@@ -164,30 +155,24 @@ public class BearAI : MonoBehaviour
         }
     }
 
-
     /// <summary>
     /// this enables the player to take damage from bullets, 
     /// ...
     /// </summary>
     /// <param name="col"></param>
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.CompareTag("PlayerAtk"))
-        {
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.CompareTag("PlayerAtk")) {
             enemyHealth--;
             //FindObjectOfType<AudioManager>().Play("SThit");
-            if (enemyHealth == 2)
-            {
+            if (enemyHealth == 2) {
                 //FindObjectOfType<AudioManager>().Play("ST2hitleft");
             }
-            if (enemyHealth == 1)
-            {
+            if (enemyHealth == 1) {
                 //FindObjectOfType<AudioManager>().Play("ST1hitleft");
             }
 
 
-            if (enemyHealth <= 0)
-            {
+            if (enemyHealth <= 0) {
                 killCount++;
                 //energy.addScore(10);
 
@@ -206,14 +191,13 @@ public class BearAI : MonoBehaviour
                 //gameObject.GetComponent<ParticleSystemRenderer>().enabled = true;   //needed or the particle sys. won't show up
                 gameObject.GetComponentInChildren<ParticleSystemRenderer>().enabled = true;   //needed or the particle sys. won't show up
                 StartExplosion();   //makes explosion occur when the enemy is hit
-                StartCoroutine(PlayAndDestroy(reloadTime));
+                StartCoroutine(PlayAndDestroy(chewTime));
             }
         }
     }
 
     //this enables the audio to play even after the object gets destroyed
-    private IEnumerator PlayAndDestroy(float waitTime)
-    {
+    private IEnumerator PlayAndDestroy(float waitTime) {
         yield return new WaitForSeconds(waitTime);
         StopExplosion();    //stops the explosion
         Destroy(gameObject);
@@ -222,35 +206,30 @@ public class BearAI : MonoBehaviour
     /// <summary>
     /// ...
     /// </summary>
-    private void StartExplosion()
-    {
-        if (explosionStarted == false)
-        {
-            explosion.Play();
-            explosionStarted = true;
+    private void StartExplosion() {
+        if (spatterStarted == false) {
+            blood.Play();
+            spatterStarted = true;
         }
     }
 
     /// <summary>
     /// ...
     /// </summary>
-    private void StopExplosion()
-    {
-        explosionStarted = false;
-        explosion.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-        //explosion.Stop();
+    private void StopExplosion() {
+        spatterStarted = false;
+        blood.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        blood.Stop();
     }
 
-    private void OnDrawGizmosSelected()
-    {
+    private void OnDrawGizmosSelected() {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, chaseDistance);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackDistance);
     }
 
-    private void OnDestroy()
-    {
+    private void OnDestroy() {
         //energy.addScore(10);
     }
 }
