2791b285cc9ee3408430fedeefc1a20a689f13b6
Wesley Johnson <C00157698@louisiana.edu>
2020-04-30 18:46:57 -0500

    Accidental Creation
    Accidentally made items in "master" instead of the branch

---
+++ Team Stairways Final Project/Assets/Scripts/InventoryHelp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Website: https://www.youtube.com/watch?v=2WnAOV7nHW0
+/// </summary>
+
+//this is a simple class, so no monobehavior elements
+public class InventoryHelp {    
+    private List<Weapons> weapons;
+}
---
+++ Team Stairways Final Project/Assets/Scripts/Weapons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Website: https://www.youtube.com/watch?v=2WnAOV7nHW0
+/// </summary>
+
+public class Weapons {
+    public enum WeapnType {
+        Sword,
+        Gun,
+        Wand,
+        RobotArms
+    }
+
+    public WeapnType equipedWeapon;
+    //public int amount; //not really needed here
+}
