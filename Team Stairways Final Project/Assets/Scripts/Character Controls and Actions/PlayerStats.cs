1cd7433873a40361fa004bc471acaeed13cd02e0
Wesley Johnson <C00157698@louisiana.edu>
2020-05-02 22:03:40 -0500

    Revert "Merge branch 'weapons_branch'"
    This reverts commit 92405cc01e33d7a1a01678c8473fbdae630ad19d, reversing
    changes made to f713f23a162c1ddd85a8e1e87f6b96cf4290656a.

--- Team Stairways Final Project/Assets/Scripts/Character Controls and Actions/PlayerStats.cs
+++ Team Stairways Final Project/Assets/Scripts/Character Controls and Actions/PlayerStats.cs
@@ -9,16 +9,6 @@ public class PlayerStats : MonoBehaviour
 {
     public static int maxHealth = 3;
     public static int playerHealth;
-    public static bool hasGun = true;
-    public static bool hasSword = true;
-    public static bool isAttacking = false;
-
-    public GameObject sword;
-    public GameObject gun;
-    public GameObject swordbox;
-    public GameObject bullet;
-    public GameObject emitter;
-    public static string Using = "";
 
     // Start is called before the first frame update
     void Start()
@@ -53,24 +43,11 @@ public class PlayerStats : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (hasGun)
-            {
-                equipmentDisplay.ChangeCurrentEquip(1);
-                gun.SetActive(true);
-                sword.SetActive(false);
-                Using = "gun";
-            }
-                
+            equipmentDisplay.ChangeCurrentEquip(1);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            if (hasSword)
-            {
-                equipmentDisplay.ChangeCurrentEquip(2);
-                sword.SetActive(true);
-                gun.SetActive(false);
-                Using = "sword";
-            }
+            equipmentDisplay.ChangeCurrentEquip(2);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
@@ -81,10 +58,6 @@ public class PlayerStats : MonoBehaviour
             equipmentDisplay.ChangeCurrentEquip(4);
         }
         
-        if (Input.GetKeyDown(KeyCode.Mouse0) && isAttacking == false)
-        {
-            Attack();
-        }
 
     }
 
@@ -94,43 +67,5 @@ public class PlayerStats : MonoBehaviour
         playerHealth += amt;
     }
 
-    //We die like men, here are the attacking scripts as well.
-
-    public void Attack()
-    {
-        isAttacking = true;
-        if(hasSword == true && Using == "sword")
-        {
-            swordbox.SetActive(true);
-            StartCoroutine(SwordHitboxActive(0.5f));
-        }
-
-        else if(hasGun == true && Using == "gun")
-        {
-            Shoot();
-            StartCoroutine(Cooldown(1.0f));
-        }
-
-    }
-
-    public IEnumerator Cooldown(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        isAttacking = false;
-    }
-
-    public IEnumerator SwordHitboxActive(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        swordbox.SetActive(false);
-        StartCoroutine(Cooldown(1.0f));
-    }
-    public void Shoot()
-    {
-        GameObject currentBullet = Instantiate(bullet, emitter.transform.position, emitter.transform.rotation) as GameObject;
-        currentBullet.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-        currentBullet.GetComponent<Rigidbody>().AddForce(transform.forward * 700);
-        Destroy(currentBullet, 3);
-    }
-
+    
 }
